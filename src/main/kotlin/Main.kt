279836980245f5c72c4c88654ef6b3e27a2b6fa6
rekkido <dikkerd@ya.ru>
2023-11-15 20:57:10 +0300

    HomeworkLesson3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+//Определение интерфейса
+interface TestRunner {
+    fun < T : TestSteps> runTest(steps: T, test: () -> Unit)
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+interface TestSteps {
+    fun beforeTest() {
+        println("Running beforeTest")
+    }
+
+    fun afterTest() {
+        println("Running afterTest")
+    }
+}
+
+// Реализация класса
+class SimpleTestRunner : TestRunner {
+    override fun <T : TestSteps> runTest(steps: T, test: () -> Unit) {
+        try {
+            steps.beforeTest()
+            test()
+        } finally {
+            steps.afterTest()
+        }
+    }
+}
+
+// Запуск тестов
+fun main() {
+    val testRunner = SimpleTestRunner()
+
+    val testSteps = object : TestSteps {
+        override fun beforeTest() {
+            println("Custom beforeTest")
+        }
+
+        override fun afterTest() {
+            println("Custom afterTest")
+        }
+    }
+
+    testRunner.runTest(testSteps) {
+        println("Running actual test")
+    }
 }
